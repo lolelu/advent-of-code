08157028d72b5738fe534d3af9757cf902af192a
lorenzocherubin <lorenzo.cherubin@sncegroup.ch>
2024-12-03 16:56:40 +0100

    Advent Of code (day 1)

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import day1Part1 from "./day1-part1";
+import day1Part2 from "./day1-part2";
+
+const main = () =>{
+    console.group("Day 1 - Part 1")
+    console.log(day1Part1)
+    console.groupEnd()
+
+    console.group("Day 1 - Part 2")
+    console.log(day1Part2)
+    console.groupEnd()
+}
+
+main()
